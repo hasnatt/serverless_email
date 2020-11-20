6b5b38a41edea92bb0218f8944e55e3b65f02dbd
hasnat <hasnat@hasnats-mbp.home>
2020-11-20 17:02:16 +0000

    reformatting email with email.message lib and make one attatchment

--- lambda_function.py
+++ lambda_function.py
@@ -1,37 +1,38 @@
 import json
 import smtplib
 import config
+from email.message import EmailMessage
+import imghdr
 
 EMAIL_ADDRESS = config.EMAIL_ADDRESS
 EMAIL_PASSWORD = config.EMAIL_PASSWORD
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    # encrypt with tls
-    smtp.starttls()
-    smtp.ehlo()
 
+msg = EmailMessage()
+msg['Subject'] = 'test email 1'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = 'hsntbdl@gmail.com'
+msg.set_content('fkgndfkgn knjfdkgfd')
+
+with open('clouds.gif', 'rb') as f:
+    file_data = f.read()
+    file_type = imghdr.what(f.name)
+    file_name = f.name
+
+msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)    
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)
 
-    subject = 'Test email'
-    body = 'fkgndfkgn knjfdkgfd'
-    message = f'Subject: {subject}\n\n{body}'
 
-    smtp.sendmail(EMAIL_ADDRESS, 'hsntbdl@gmail.com', message)
 
 # def lambda_handler(event, context):
-
 #     return {
 #         'statusCode': 200,
-#         'body': json.dumps('Hello from Lambda!')
-#     }
-
-
+#         'body': json.dumps('Hello from Lambda!')}
 
 # if __name__ == "__main__":
-
-#     event ={
-
-#     }
+#     event ={}
 #     context = []
 #     print(lambda_handler(event, context))
