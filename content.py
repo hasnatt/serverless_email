e12ca7bf61e03e7269eb945c5ed30fd1456c9979
hasnat <hasnat@hasnats-mbp.home>
2020-11-20 23:04:38 +0000

    Jinja2 implemented with Environment and FileSystemLoader

--- content.py
+++ content.py
@@ -1,15 +1,12 @@
-text = 'EMAIL'
+# from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
+env = Environment(loader=FileSystemLoader(''))
+template = env.get_template('template.html')
 
-HTML_CONTENT = f"""\
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-</head>
-<body>
- <h1>{text}</h1>
-</body>
-</html>
-"""
 
-# look at
-# https://techmonger.github.io/69/jinja2-render-html/
+output_from_parsed_template = template.render(name='Hasnat!')
+print(output_from_parsed_template)
+
+# to save the results
+with open("email.html", "w") as fh:
+    fh.write(output_from_parsed_template)
