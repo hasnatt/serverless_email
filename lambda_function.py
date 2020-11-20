0bd35958489bed92333c194d4fe3ffb3dd3f896e
hasnat <hasnat@hasnats-mbp.home>
2020-11-20 16:24:05 +0000

    smtp setup

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,37 @@
 import json
+import smtplib
+import config
 
-def lambda_handler(event, context):
+EMAIL_ADDRESS = config.EMAIL_ADDRESS
+EMAIL_PASSWORD = config.EMAIL_PASSWORD
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    # encrypt with tls
+    smtp.starttls()
+    smtp.ehlo()
+
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+    subject = 'Test email'
+    body = 'fkgndfkgn knjfdkgfd'
+    message = f'Subject: {subject}\n\n{body}'
+
+    smtp.sendmail(EMAIL_ADDRESS, 'hsntbdl@gmail.com', message)
+
+# def lambda_handler(event, context):
+
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps('Hello from Lambda!')
+#     }
 
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    event ={
+#     event ={
 
-    }
-    context = []
-    print(lambda_handler(event, context))
+#     }
+#     context = []
+#     print(lambda_handler(event, context))
