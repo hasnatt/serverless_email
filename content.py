fd109e1d476db1ac44ea6ed41d50c9a203fd3d49
hasnat <hasnat@hasnats-mbp.home>
2020-11-20 23:26:38 +0000

    new memes!!

--- content.py
+++ content.py
@@ -1,12 +1,10 @@
-# from jinja2 import Template
 from jinja2 import Environment, FileSystemLoader
+
 env = Environment(loader=FileSystemLoader(''))
 template = env.get_template('template.html')
-
-
-output_from_parsed_template = template.render(name='Hasnat!')
-print(output_from_parsed_template)
+HTML_CONTENT = template.render(name='Hasnat!')
+print(HTML_CONTENT)
 
 # to save the results
-with open("email.html", "w") as fh:
-    fh.write(output_from_parsed_template)
+# with open("email.html", "w") as fh:
+#     fh.write(output_from_parsed_template)
--- lambda_function.py
+++ lambda_function.py
@@ -10,30 +10,28 @@ from jinja2 import Template
 
 EMAIL_ADDRESS = config.EMAIL_ADDRESS
 EMAIL_PASSWORD = config.EMAIL_PASSWORD
+EMAIL_TO = config.EMAIL_TO
 HTML_CONTENT = content.HTML_CONTENT
 
-
+# construct email
 msg = EmailMessage()
 msg['Subject'] = 'Images'
 msg['From'] = EMAIL_ADDRESS
-msg['To'] = 'hsntbdl@gmail.com'
-# msg.set_content('Plain text email')
-
+msg['To'] = EMAIL_TO
 msg.set_content(HTML_CONTENT, subtype = 'html')
 
-
+# add attatchments
 attatchment_files = os.listdir('attatchments/')
-
 for filename in attatchment_files:
     file = os.path.join('attatchments/', filename)
     with open(file, 'rb') as f:
         file_data = f.read()
         file_type = imghdr.what(f.name)
-        print(file_type)
         file_name =os.path.basename(f.name)
-
+    # add attachmnt to the message
     msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)    
 
+# SMTP send email
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
     smtp.send_message(msg)
