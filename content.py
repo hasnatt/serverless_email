f514bef84a83658340685e7aa2936827c5073582
hasnat <hasnat@hasnats-mbp.home>
2020-11-21 13:32:18 +0000

    allow for dictionary input to render html

--- content.py
+++ content.py
@@ -1,10 +1,17 @@
 from jinja2 import Environment, FileSystemLoader
 
+# put data into dictinary
+name = 'hasnat'
+body = {
+    'name': name
+}
+
+# open file env and render emplate with dictionary
 env = Environment(loader=FileSystemLoader(''))
 template = env.get_template('template.html')
-HTML_CONTENT = template.render(name='Hasnat!')
+HTML_CONTENT = template.render(body=body)
 print(HTML_CONTENT)
 
-# to save the results
+# save the html
 # with open("email.html", "w") as fh:
 #     fh.write(output_from_parsed_template)
